081c727d8ab993bfbdfabb6b0696e6166a89f748
mohammad eunus <38430781+mohammadeunus@users.noreply.github.com>
2021-10-24 02:04:15 +0600

    11

---
+++ datacamp.com/1.introduction to python/4.numpy/05.gk.py
@@ -0,0 +1,9 @@
+'''
+here True mean 1 and False mean 0
+'''
+import numpy as np
+np.array([True, 1, 2]) + np.array([3, 4, False])
+'''
+and the following code are same 
+np.array([ 01 , 1, 2]) + np.array([3, 4,  00  ])  
+'''
